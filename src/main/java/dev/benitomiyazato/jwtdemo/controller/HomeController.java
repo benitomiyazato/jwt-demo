f31a91d7ebab5be7e9b7986bdbfeeac41c93140d
bemiyaz <benito.miya172@gmail.com>
2023-01-25 19:28:48 -0300

    SecurityConfig

--- src/main/java/dev/benitomiyazato/jwtdemo/controller/HomeController.java
+++ src/main/java/dev/benitomiyazato/jwtdemo/controller/HomeController.java
@@ -1,13 +1,17 @@
 package dev.benitomiyazato.jwtdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping()
-    public String home(){
-        return "Hello, World!";
+    @GetMapping
+    public String home(Principal principal){
+        return "Hello, World!" + "PRINCIPAL NAME: " + principal.getName();
     }
 }
